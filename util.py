cff42c2675fa8ff7a27003881487abb6045cbd0a
ihsansecer <abdullahsecer@std.sehir.edu.tr>
2017-11-16 12:44:00 +0300

    Add authentication function

---
+++ util.py
@@ -0,0 +1,10 @@
+import json
+
+import tweepy
+
+
+def authenticate():
+    auth_tokens = json.load(open("auth_tokens.json"))
+    auth = tweepy.OAuthHandler(auth_tokens["consumer_key"], auth_tokens["consumer_secret"])
+    auth.set_access_token(auth_tokens["access_token"], auth_tokens["access_token_secret"])
+    return tweepy.API(auth)
