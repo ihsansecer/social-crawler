c6de5e5e0d4af8f5b514618081048bc7b6295c35
ihsansecer <abdullahsecer@std.sehir.edu.tr>
2017-11-16 23:35:48 +0300

    #1: Make crawler wait when rate limit exceeded

--- util.py
+++ util.py
@@ -7,4 +7,4 @@ def authenticate():
     auth_tokens = json.load(open("auth_tokens.json"))
     auth = tweepy.OAuthHandler(auth_tokens["consumer_key"], auth_tokens["consumer_secret"])
     auth.set_access_token(auth_tokens["access_token"], auth_tokens["access_token_secret"])
-    return tweepy.API(auth)
+    return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
