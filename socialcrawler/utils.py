76f0ea69181697ad4d2f496e34f55c01c456e137
ihsansecer <abdullahsecer@std.sehir.edu.tr>
2017-11-29 23:52:34 +0300

    #8: Setup database orm and migration tool

--- socialcrawler/utils.py
+++ socialcrawler/utils.py
@@ -1,13 +1,19 @@
+from collections import namedtuple
 import json
 
 import tweepy
 
 
 def init_twitter_api():
-    auth_tokens = read_file("config.json")["twitter"]["auth"]
-    auth = tweepy.OAuthHandler(auth_tokens["consumer_key"], auth_tokens["consumer_secret"])
-    auth.set_access_token(auth_tokens["access_token"], auth_tokens["access_token_secret"])
+    auth_tokens = get_config().twitter.auth
+    auth = tweepy.OAuthHandler(auth_tokens.consumer_key, auth_tokens.consumer_secret)
+    auth.set_access_token(auth_tokens.access_token, auth_tokens.access_token_secret)
     return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+def get_config():
+    with open("config.json", 'r') as f:
+        return json.load(f, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
 
 def read_file(path):
