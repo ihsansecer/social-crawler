9c9e50568d2a4c3a842a9d810604c3e8fa499351
ihsansecer <abdullahsecer@std.sehir.edu.tr>
2017-12-28 20:16:32 +0300

    Handle Cursor iterator errors

--- socialcrawler/crawlers.py
+++ socialcrawler/crawlers.py
@@ -35,12 +35,7 @@ class UserCrawler(object):
 
     def _fetch_connection_ids(self, user_id, connection_type):
         connection_fetcher = getattr(self._api, "{}_ids".format(connection_type))
-        try:
-            return tweepy.Cursor(connection_fetcher, id=user_id).items()
-        except tweepy.TweepError as e:
-            if e.api_code == 179:
-                print("not authorized to see {} of user, {}.".format(connection_type, self._user_id))
-            return []
+        return tweepy.Cursor(connection_fetcher, id=user_id).items()
 
     def _fetch_user(self, user_id):
         try:
@@ -52,22 +47,32 @@ class UserCrawler(object):
 
     def _crawl_connections(self, connection_type, user_id, depth, con_limit):
         connection_ids = self._fetch_connection_ids(user_id, connection_type)
-        for connection_id in connection_ids:
-            if not row_exist(self._session, TwitterUser, id=connection_id):
-                user = self._fetch_user(connection_id)
-                if not user:
-                    continue
-                self._create_user(user)
-            if connection_type == "friends" and \
-                    not row_exist(self._session, TwitterConnection, from_user_id=user_id, to_user_id=connection_id):
-                self._create_connection(user_id, connection_id)
-            elif connection_type == "followers" and \
-                    not row_exist(self._session, TwitterConnection, from_user_id=connection_id, to_user_id=user_id):
-                self._create_connection(connection_id, user_id)
+        while True:
+            try:
+                connection_id = connection_ids.next()
+                if not row_exist(self._session, TwitterUser, id=connection_id):
+                    user = self._fetch_user(connection_id)
+                    if not user:
+                        continue
+                    self._create_user(user)
+                self._crawl_connection(connection_type, user_id, depth, con_limit, connection_id)
+            except tweepy.TweepError:
+                print("not authorized to see {} of user, {}.".format(connection_type, self._user_id))
+                return
+            except StopIteration:
+                return
 
-            if depth > 1:
-                crawler = UserCrawler(self._api, self._session, connection_id)
-                crawler.crawl(depth - 1, con_limit)
+    def _crawl_connection(self, connection_type, user_id, depth, con_limit, connection_id):
+        if connection_type == "friends" and \
+                not row_exist(self._session, TwitterConnection, from_user_id=user_id, to_user_id=connection_id):
+            self._create_connection(user_id, connection_id)
+        elif connection_type == "followers" and \
+                not row_exist(self._session, TwitterConnection, from_user_id=connection_id, to_user_id=user_id):
+            self._create_connection(connection_id, user_id)
+
+        if depth > 1:
+            crawler = UserCrawler(self._api, self._session, connection_id)
+            crawler.crawl(depth - 1, con_limit)
 
     def _crawl_friends(self, *args):
         self._crawl_connections("friends", *args)
@@ -97,17 +102,22 @@ class UserTweetCrawler(object):
         self._user_id = user_id
         self._session = session
 
+    def _create_entry(self, tweet):
+        self._session.merge(TwitterEntry(
+            id=tweet.id,
+            user_id=self._user_id,
+            text=tweet.text
+        ))
+        self._session.commit()
+
     def crawl(self):
-        try:
-            tweets = tweepy.Cursor(self._api.user_timeline, id=self._user_id).items()
-            for tweet in tweets:
-                self._session.merge(TwitterEntry(
-                    id=tweet.id,
-                    user_id=self._user_id,
-                    text=tweet.text
-                ))
-            self._session.commit()
-        except tweepy.TweepError as e:
-            if e.api_code == 179:
+        cursor = tweepy.Cursor(self._api.user_timeline, id=self._user_id).items()
+        while True:
+            try:
+                tweet = cursor.next()
+                self._create_entry(tweet)
+            except tweepy.TweepError:
                 print("not authorized to see tweets of user, {}.".format(self._user_id))
-            return []
+                return
+            except StopIteration:
+                return
