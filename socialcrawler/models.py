c1cc4a2318138bda45e57086e3ede80d289b3aac
ihsansecer <abdullahsecer@std.sehir.edu.tr>
2018-04-01 19:51:26 +0300

    #11: Model connection change and crawler run data

--- socialcrawler/models.py
+++ socialcrawler/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, BigInteger, Text
+import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, BigInteger, Text, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -30,6 +32,17 @@ class TwitterConnection(Base):
     to_user = relationship("TwitterUser", foreign_keys=[to_user_id])
 
 
+class TwitterConnectionChange(Base):
+    __tablename__ = 'twitter_connection_change'
+
+    id = Column(Integer, primary_key=True)
+    is_added = Column(Boolean, nullable=False)
+    connection_id = Column(ForeignKey("twitter_connection.id"), nullable=False)
+    connection = relationship("TwitterConnection", foreign_keys=[connection_id])
+    run_id = Column(ForeignKey("crawler_run.id"), nullable=False)
+    run = relationship("CrawlerRun", foreign_keys=[run_id])
+
+
 class TwitterEntry(Base):
     __tablename__ = 'twitter_entry'
 
@@ -37,3 +50,10 @@ class TwitterEntry(Base):
     user_id = Column(ForeignKey("twitter_user.id"))
     user = relationship("TwitterUser", foreign_keys=[user_id])
     text = Column(String(280))
+
+
+class CrawlerRun(Base):
+    __tablename__ = 'crawler_run'
+
+    id = Column(BigInteger, primary_key=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
